923c3e05afc70796f6fef586fdcd67eb13be39fd
Tal Derei <talderei99@gmail.com>
2023-10-22 13:29:07 -0700

    Initial commit [boilerplate]

---
+++ jest.config.ts
@@ -0,0 +1,13 @@
+export default {
+  // Automatically clear mock calls, instances, contexts and results before every test
+  clearMocks: true,
+
+  // Indicates whether the coverage information should be collected while executing the test
+  collectCoverage: false,
+
+  // The directory where Jest should output its coverage files
+  coverageDirectory: "coverage",
+
+  // Amount of time (in milliseconds) to wait for hook in test
+  testTimeout: 60000,
+};
